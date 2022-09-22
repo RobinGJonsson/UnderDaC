7957e779e4f256ff26a39255cab376646db88f78
RobinGJonsson <robin__94@hotmail.se>
2022-09-22 14:23:16 +0000

    Update css file in cloudinary

--- underdac/settings.py
+++ underdac/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
@@ -175,5 +175,4 @@ EMAIL_USE_TLS = False
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = 1025
 
-# AUTH_USER_MODEL = 'restaurant.Customer'
 ACCOUNT_SIGNUP_FORM_CLASS = 'restaurant.forms.SignupForm'
