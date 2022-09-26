6d688905b1b19e9d1675ec091de2fd4e07e2501f
RobinGJonsson <robin__94@hotmail.se>
2022-09-26 12:26:10 +0000

    Change banner size in home page

--- underdac/settings.py
+++ underdac/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 import dj_database_url
 from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
@@ -109,16 +110,10 @@ WSGI_APPLICATION = 'underdac.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-'''DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}'''
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL')),
+    }
 
 
 # Password validation
