f2211237059d88633c57008bcae6e3793279ed95
RobinGJonsson <robin__94@hotmail.se>
2022-09-29 06:33:22 +0000

    Add auto scroll on updte booking p

--- static/js/base.js
+++ static/js/base.js
@@ -1,22 +1,26 @@
+// Auto scroll down to the form section if user wants to update booking
+let pageURL = window.location.href;
 
+if (pageURL.includes('update_booking')) {
+  document.querySelector('#form-section').scrollIntoView(alignToTop = true);
+}
 
 // Card flip activation for maps in homepage
 let mapBtns = document.querySelectorAll('.map-btn');
 
 for (let btn of mapBtns) {
-    btn.addEventListener('click', function() {
-        let mapId = this.dataset.mapnum;
-        cardId = 'flip-card-inner-' + mapId;
+  btn.addEventListener('click', function () {
+    let mapId = this.dataset.mapnum;
+    cardId = 'flip-card-inner-' + mapId;
 
-        card = document.getElementById(cardId);
+    card = document.getElementById(cardId);
 
-        if (card.classList.contains('flip')) {
-            card.classList.remove('flip');
-        }
-        else {
-            card.classList.add('flip');
-        }
-    })
+    if (card.classList.contains('flip')) {
+      card.classList.remove('flip');
+    } else {
+      card.classList.add('flip');
+    }
+  })
 }
 
 //Auto close bootstrap messages
@@ -31,34 +35,32 @@ if (alertWindow.length > 0) {
 }
 
 function getCookie(name) {
-    // Split cookie string and get all individual name=value pairs in an array
-    let cookieArr = document.cookie.split(";");
-  
-    // Loop through the array elements
-    for (let i = 0; i < cookieArr.length; i++) {
-      let cookiePair = cookieArr[i].split("=");
-  
-      /* Removing whitespace at the beginning of the cookie name
-      and compare it with the given string */
-      if (name == cookiePair[0].trim()) {
-        // Decode the cookie value and return
-        return decodeURIComponent(cookiePair[1]);
-      }
+  // Split cookie string and get all individual name=value pairs in an array
+  let cookieArr = document.cookie.split(";");
+
+  // Loop through the array elements
+  for (let i = 0; i < cookieArr.length; i++) {
+    let cookiePair = cookieArr[i].split("=");
+
+    /* Removing whitespace at the beginning of the cookie name
+    and compare it with the given string */
+    if (name == cookiePair[0].trim()) {
+      // Decode the cookie value and return
+      return decodeURIComponent(cookiePair[1]);
     }
-  
-    // Return null if not found
-    return null; 
   }
-  
+
+  // Return null if not found
+  return null;
+}
+
 let cart = JSON.parse(getCookie('cart'));
 
 //If there is no cart, set it to an empty object
-if (cart == undefined){
+if (cart == undefined) {
   cart = {};
   console.log('Cart Created!', cart);
-  document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/";
+  document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/";
 }
 
 console.log('Cart:', cart)
-
-
